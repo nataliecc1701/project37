fa659f87a539285513fa143d6cc263ac42bfab43
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-06-17 10:39:11 -0700

    imported forbiddenerror

--- middleware/auth.test.js
+++ middleware/auth.test.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const jwt = require("jsonwebtoken");
-const { UnauthorizedError } = require("../expressError");
+const { UnauthorizedError, ForbiddenError } = require("../expressError");
 const {
   authenticateJWT,
   ensureLoggedIn,
