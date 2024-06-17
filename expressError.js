4790427285726f05262ae9ca8f9a5b5c4edea379
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-06-17 10:32:32 -0700

    corrected comment on ForbiddenError

--- expressError.js
+++ expressError.js
@@ -36,7 +36,7 @@ class BadRequestError extends ExpressError {
   }
 }
 
-/** 403 BAD REQUEST error. */
+/** 403 FORBIDDEN error. */
 
 class ForbiddenError extends ExpressError {
   constructor(message = "Bad Request") {
