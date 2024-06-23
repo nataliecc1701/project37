3ce46fa1f36978470cd9e37e6603943cce8a6b37
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-06-23 00:17:16 -0700

    tests for GET request

--- routes/jobs.test.js
+++ routes/jobs.test.js
@@ -72,4 +72,112 @@ describe("POST /jobs", function () {
           .set("authorization", `Bearer ${adminToken}`);
       expect(resp.statusCode).toEqual(400);
     });
-  });
+});
+
+/************************************** GET /companies */
+
+describe("GET /jobs", function () {
+    test("ok for anon", async function () {
+      const resp = await request(app).get("/companies");
+      expect(resp.body).toEqual({
+        companies:
+            [
+                {
+                    id: expect.any(Number),
+                    title: "j1",
+                    salary: 1,
+                    equity: "0",
+                    companyHandle: "c1"
+                },
+                {
+                    id: expect.any(Number),
+                    title: "j2",
+                    salary: 2,
+                    equity: "1",
+                    companyHandle: "c2"
+                },
+                {
+                    id: expect.any(Number),
+                    title: "j3",
+                    salary: 3,
+                    equity: "0.5",
+                    companyHandle: "c3"
+                  },
+            ],
+      });
+    });
+    
+    // test("runs with name parameter", async function () {
+    //   const resp = await request(app).get("/companies").query({ name: "c1" });
+    //   expect(resp.body).toEqual({
+    //     companies:
+    //       [
+    //         {
+    //           handle: "c1",
+    //           name: "C1",
+    //           description: "Desc1",
+    //           numEmployees: 1,
+    //           logoUrl: "http://c1.img",
+    //         }
+    //       ]
+    //   });
+    // })
+    
+    // test("runs with min and max parameters", async function () {
+    //   const resp = await request(app).get("/companies")
+    //     .query({ minEmployees: 2, maxEmployees: 2});
+    //   expect(resp.body).toEqual({
+    //     companies:
+    //       [
+    //         {
+    //           handle: "c2",
+    //           name: "C2",
+    //           description: "Desc2",
+    //           numEmployees: 2,
+    //           logoUrl: "http://c2.img",
+    //         },
+    //       ]
+    //   });
+    // })
+    
+    // test("runs with bogus parameters", async function () {
+    //   const resp = await request(app).get("/companies").query({ bogus: "foo" });
+    //   expect(resp.body).toEqual({
+    //     companies:
+    //         [
+    //           {
+    //             handle: "c1",
+    //             name: "C1",
+    //             description: "Desc1",
+    //             numEmployees: 1,
+    //             logoUrl: "http://c1.img",
+    //           },
+    //           {
+    //             handle: "c2",
+    //             name: "C2",
+    //             description: "Desc2",
+    //             numEmployees: 2,
+    //             logoUrl: "http://c2.img",
+    //           },
+    //           {
+    //             handle: "c3",
+    //             name: "C3",
+    //             description: "Desc3",
+    //             numEmployees: 3,
+    //             logoUrl: "http://c3.img",
+    //           },
+    //         ],
+    //   })
+    // })
+  
+    test("fails: test next() handler", async function () {
+      // there's no normal failure event which will cause this route to fail ---
+      // thus making it hard to test that the error-handler works with it. This
+      // should cause an error, all right :)
+      await db.query("DROP TABLE companies CASCADE");
+      const resp = await request(app)
+          .get("/companies")
+          .set("authorization", `Bearer ${u1Token}`);
+      expect(resp.statusCode).toEqual(500);
+    });
+});
