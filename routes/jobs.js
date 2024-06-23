5de2720e89047a4a99a92e107b4be750496682cf
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-06-22 23:28:07 -0700

    created files for jobs and jobs test cases

---
+++ routes/jobs.js
@@ -0,0 +1,15 @@
+"use strict";
+
+/** Routes for companies. */
+
+const jsonschema = require("jsonschema");
+const express = require("express");
+
+const { BadRequestError } = require("../expressError");
+const { ensureLoggedIn, ensureIsAdmin } = require("../middleware/auth");
+const Company = require("../models/company");
+
+const companyNewSchema = require("../schemas/companyNew.json");
+const companyUpdateSchema = require("../schemas/companyUpdate.json");
+
+const router = new express.Router();
---
+++ routes/jobs.test.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const request = require("supertest");
+
+const db = require("../db");
+const app = require("../app");
+
+const {
+  commonBeforeAll,
+  commonBeforeEach,
+  commonAfterEach,
+  commonAfterAll,
+  u1Token,
+  adminToken,
+} = require("./_testCommon");
+
+beforeAll(commonBeforeAll);
+beforeEach(commonBeforeEach);
+afterEach(commonAfterEach);
+afterAll(commonAfterAll);
