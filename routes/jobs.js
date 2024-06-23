502687035dfd83d0dec617039b41fd57a7528156
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-06-22 23:53:57 -0700

    added POST route

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,15 +1,41 @@
 "use strict";
 
-/** Routes for companies. */
+/** Routes for jobs. */
 
 const jsonschema = require("jsonschema");
 const express = require("express");
 
 const { BadRequestError } = require("../expressError");
 const { ensureLoggedIn, ensureIsAdmin } = require("../middleware/auth");
-const Company = require("../models/company");
+const Job = require("../models/job");
 
-const companyNewSchema = require("../schemas/companyNew.json");
-const companyUpdateSchema = require("../schemas/companyUpdate.json");
+const jobNewSchema = require("../schemas/jobNew.json");
+const jobUpdateSchema = require("../schemas/jobUpdate.json");
 
 const router = new express.Router();
+
+/** POST / { job } =>  { job }
+ *
+ * job should be { title, salary, equity, companyHandle }
+ *
+ * Returns { id, title, salary, equity, companyHandle }
+ *
+ * Authorization required: admin
+ */
+
+router.post("/", ensureIsAdmin, async function (req, res, next) {
+    try {
+      const validator = jsonschema.validate(req.body, jobNewSchema);
+      if (!validator.valid) {
+        const errs = validator.errors.map(e => e.stack);
+        throw new BadRequestError(errs);
+      }
+  
+      const job = await Job.create(req.body);
+      return res.status(201).json({ job });
+    } catch (err) {
+      return next(err);
+    }
+});
+
+module.exports = router;
