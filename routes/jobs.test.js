bc732464905e95d024cef0b7c0fdea3bbaa263b1
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-06-22 23:39:43 -0700

    tests set up for POST /jobs

--- routes/jobs.test.js
+++ routes/jobs.test.js
@@ -18,3 +18,58 @@ beforeAll(commonBeforeAll);
 beforeEach(commonBeforeEach);
 afterEach(commonAfterEach);
 afterAll(commonAfterAll);
+
+/************************************** POST /companies */
+
+describe("POST /jobs", function () {
+    const newJob = {
+      title: "new",
+      company_handle: "c1",
+      salary: 10,
+      equity: "0.75"
+    };
+  
+    test("ok for admins", async function () {
+      const resp = await request(app)
+          .post("/jops")
+          .send(newJob)
+          .set("authorization", `Bearer ${adminToken}`);
+      expect(resp.statusCode).toEqual(201);
+      expect(resp.body).toEqual({
+        job: {
+            id: expect.any(Number),
+            ...newJob
+            },
+      });
+    });
+    
+    test("forbidden for non-admin users", async function () {
+      const resp = await request(app)
+          .post("/jobs")
+          .send(newJob)
+          .set("authorization", `Bearer ${u1Token}`);
+      expect(resp.statusCode).toEqual(403);
+    });
+  
+    test("bad request with missing data", async function () {
+      const resp = await request(app)
+          .post("/jobs")
+          .send({
+            title: "new",
+            salary: 10,
+          })
+          .set("authorization", `Bearer ${adminToken}`);
+      expect(resp.statusCode).toEqual(400);
+    });
+  
+    test("bad request with invalid data", async function () {
+      const resp = await request(app)
+          .post("/jobs")
+          .send({
+            ...newJob,
+            salary: -1,
+          })
+          .set("authorization", `Bearer ${adminToken}`);
+      expect(resp.statusCode).toEqual(400);
+    });
+  });
